b896d0f86af59becb28290fb8b6ca37d263b9e2b
Choclos15 <mario221649@gmail.com>
2022-12-14 14:19:13 -0700

    no pude poner en verde el check

--- src/App.js
+++ src/App.js
@@ -19,15 +19,12 @@ function App() {
   return (
     <React.Fragment>
       <TodoCounter/>
-      {/*<h2>2 de 3 ToDo's Completados</h2>*/}
 
       <TodoSearch/>
-      {/*<input placeholder='Cebolla'/>*/}
       <TodoList>
         {todos.map(todo => (<TodoItem key={todo.text} text={todo.text}/>))}
       </TodoList>
       <CreateTodoButton/>
-      {/*<button>+</button>*/}
     </React.Fragment>
   );
 }
