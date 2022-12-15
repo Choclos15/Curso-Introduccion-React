c4d26b24c0e206d642ae4a1972d7f2eeaef0dfa2
Choclos15 <mario221649@gmail.com>
2022-12-15 11:43:11 -0700

    commit evento onClick y onChange

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,9 +1,17 @@
 import React from "react";
 import './CreateTodoButton.css'
 
-function CreateTodoButton() {
+function CreateTodoButton(props) {
+    const onClickButton = (msg) => {
+        alert(msg)
+    }
     return (
-        <button className="CreateTodoButton">+</button>
+        <button 
+        className="CreateTodoButton"
+        onClick={() => onClickButton('Hola')}
+        >
+            +
+        </button>
     );
 }
 
