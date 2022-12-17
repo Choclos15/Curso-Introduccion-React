acb6b5ed73adf1d4142c41347ae6db2f5bffeaf4
Choclos15 <mario221649@gmail.com>
2022-12-17 11:33:23 -0700

    commit github

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.css'
-
+//Viendo si funciona con GitHub
 function Modal( {children} ) {
     return ReactDOM.createPortal(
         <div className='ModalBackground'>
